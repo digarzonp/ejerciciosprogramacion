553eb4b24deaf298014c9a1c86dabc0080e08338
daniela irene garzon pinto <digarzonp@unal.edu.co>
2018-03-18 10:34:35 -0500

    un ejemplo de cadena con string

---
+++ string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+  string a;
+  a= "este es un string inicial";
+  cout <<a<<endl;
+  a = "este es un string diferente";
+  cout <<a <<endl;
+
+  return 0;
+}
