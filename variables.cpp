b5f83e352c6ac08781e4d089f524f0a34e83160e
daniela irene garzon pinto <digarzonp@unal.edu.co>
2018-03-18 10:11:21 -0500

    se crea programa que muestra como se usan las variables

---
+++ variables.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+
+{
+  int a, b;
+  int result;
+
+  a= 5;
+  b= 2;
+  a=a+1;
+  result= a - b;
+
+  cout<< result;
+  
+  return 0;
+}
