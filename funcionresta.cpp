fba238e3317b4e93bec7be606db1d6b2a935dcf8
daniela irene garzon pinto <digarzonp@unal.edu.co>
2018-03-18 12:20:33 -0500

    funcion de resta

---
+++ funcionresta.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int resta(int a,int b)
+{
+  int r;
+  r=a-b;
+    return r;
+}
+
+int main()
+{
+   int d;  
+  int c;
+  int m;
+  cin>>c;
+  cin>>d;
+ 
+  m=resta(c,d);
+  cout<<"la resta es"<<m<<"\n";  
+}
