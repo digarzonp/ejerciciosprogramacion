eb23f8f3661370845a82c530cbb556fb959fa582
daniela irene garzon pinto <digarzonp@unal.edu.co>
2018-03-18 09:38:13 -0500

    imprimir hola mundo

---
+++ holamundo.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+  char name[150];
+  cout<< "hola mundo" <<endl;
+  cin>> name;
+  cout << name;
+  cout << "hello, w" << "o" << "r"<< "ld" <<'\n';
+    
+}
